efc4b0af372f0b93e6c4a6b6ef26ace7d8911b87
colburncodes <colburnsanders@gmail.com>
2021-03-19 23:00:16 -0500

    ui mock up register/login forms

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -1,9 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Login.css";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import { GithubLoginButton } from "react-social-login-buttons";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = () => {
+    alert("A new user was registered");
+  };
+
+  const loginForm = () => (
+    <Form className="login-form" onSubmit={handleSubmit}>
+      <div className="form-group">
+        <FormGroup>
+          <label>Username or email address</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </FormGroup>
+        <Button className="btn-lg btn-block" color="info">
+          Login
+        </Button>
+        <div className="text-center pt-3">
+          Or continue with your social account
+        </div>
+        <GithubLoginButton />
+        <div className="text-center">
+          <Link>Sign up</Link>
+          <span className="p-2">|</span>
+          <Link>Forgot Password</Link>
+        </div>
+      </div>
+    </Form>
+  );
+
   return (
-    <div className="container-fluid login h1 p-5 text-center">Login Page</div>
+    <>
+      <div className="container-fluid bg-warning p-5 text-center">
+        <span className="font-weight-bold">
+          <h1 className="text-center">OPENSOURCE:DEV</h1>
+          <h5>Sign in to OPENSOURCE:DEV</h5>
+        </span>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 offset-md-3">{loginForm()}</div>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/auth/Register.js
+++ src/auth/Register.js
@@ -1,8 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Register.css";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import {
+  FacebookLoginButton,
+  GithubLoginButton,
+} from "react-social-login-buttons";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = () => {
+    alert("A new user was registered");
+  };
+
+  const registerForm = () => (
+    <Form className="register-form" onSubmit={handleSubmit}>
+      <div className="form-group">
+        <FormGroup>
+          <label>UserName</label>
+          <input
+            type="name"
+            className="form-control"
+            placeholder="username"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label>Email</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="email address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup>
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </FormGroup>
+        <Button className="btn-lg btn-block" color="info">
+          Signup
+        </Button>
+        <Button className="btn-lg btn-dark btn-block">Cancel</Button>
+      </div>
+    </Form>
+  );
+
   return (
-    <div className="container-fluid h1 p-5 text-center">Register Page</div>
+    <>
+      <div className="container-fluid bg-warning p-5 text-center">
+        <span className="font-weight-bold">
+          <h5 className="text-center">Join OPENSOURCE:DEV</h5>
+          <h1>Create your account</h1>
+        </span>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 offset-md-3">{registerForm()}</div>
+        </div>
+      </div>
+    </>
   );
 };
 
